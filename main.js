6a1b84c6eebb389655552bc7007720e9a25eb6fe
Ekaterina Kozhara <ekaterina.kozhara@yandex.ru>
2019-05-22 08:47:55 +0300

    1

---
+++ main.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('button')
+let nav = document.querySelector('nav')
+
+function toggleMenuVisible() {
+ nav.classList.toggle('is-hidden')
+}
+button.onclick = toggleMenuVisible
